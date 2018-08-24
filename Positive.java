e14377f7247fcce47d560225189ccc2784ab633d
Naveen21Prasath <42665370+Naveen21Prasath@users.noreply.github.com>
2018-08-24 14:02:14 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Positive
+{
+ public static void main(String args[])
+ {
+ Scanner s=new Scanner(System.in)
+ int i=s.nextInt();
+ if(i>0)
+ {
+  System.out.print(pos);
+  }
+  else if(i==0)
+  {
+  System.out.print(zero);
+  }
+  else
+  {
+  System.out.print(neg);
+  }
+  }
